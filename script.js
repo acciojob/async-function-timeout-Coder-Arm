58a93f181ee6375b8bbd4cde4bbf0b42eba002fe
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-08 20:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+async function display(){
+	// event.preventDefault();
+	let text = document.getElementById("text").value;
+let delay = document.getElementById("delay").value;
+  const response = await new Promise((resolve) => {
+	  setTimeout(() => {
+		  output.innerText = text;
+	  },delay*1000)
+  });
+	return response;
+}
+btn.addEventListener("click",display);
